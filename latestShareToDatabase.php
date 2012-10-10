0b49edbb8c273558b1cf62f825e6b65265be86d7
Jørgen Jacobsen <jorgeja@gmail.com>
2012-10-10 18:22:41 +0200

    Fixes 10.10.2012

--- latestShareToDatabase.php
+++ latestShareToDatabase.php
@@ -7,8 +7,10 @@ require_once("../databaseconnection.php");
 require_once("lib/isauthenticated.php");
 if(!isauthenticated())
 	exit("1");
-
-$lastSeenShareId = $_GET['id'];
+$lastSeenShareId = false;
+if($_GET['id']) {
+	$lastSeenShareId = $_GET['id'];
+}
 
 $twitterObj = new EpiTwitter($consumer_key, $consumer_secret, $_COOKIE['oauth_token'], $_COOKIE['oauth_token_secret']);
 $twitterInfo= $twitterObj->get_accountVerify_credentials();
@@ -16,17 +18,23 @@ $result = mysql_query("select id from user where screen_name='" . $twitterInfo->
 $user = mysql_fetch_assoc($result);
 
 
+
+
 $result = mysql_query("select userid, shareid from latestshareview where userid = " . $user['id'] . ";");
 
-if(mysql_num_rows($result) > 0) {
-	mysql_query("update latestshareview set shareid = " . $lastSeenShareId . " where userid = " . $user['id']. ";");	
-	echo "0";
-}
-else {
-	if(!$lastSeenShareId) echo $lastSeenShareId;
+	if(!$lastSeenShareId) {
+		$lastSeenShareIdQuery = mysql_fetch_assoc($result);
+		echo $lastSeenShareIdQuery['shareid'];
+		exit();
+	}	
 	else {
-			mysql_query("insert into latestshareview values(''," . $lastSeenShareId . "," . $user['id']. ",null);");
-}		}	
+		if(mysql_num_rows($result) > 0) {
+			mysql_query("update latestshareview set shareid = " . $lastSeenShareId . " where userid = " . $user['id']. ";");
+		}
+		else {
+					mysql_query("insert into latestshareview values(''," . $lastSeenShareId . "," . $user['id']. ",null);");
+		}
+}	
 mysql_close($connection); 
 
 exit("0");
