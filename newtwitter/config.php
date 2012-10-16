edf9c4e56804553ce1c2c7c7ab540f3b84a4fecd
Jørgen Jacobsen <jorgeja@gmail.com>
2012-10-16 17:17:21 +0200

    hepp

--- newtwitter/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Replace XXX with your twitter application variables.
-define('CONSUMER_KEY', 'XXX');
-define('CONSUMER_SECRET', 'XXX');
-define('OAUTH_CALLBACK', 'http://XXX.com/process.php');
-?>
